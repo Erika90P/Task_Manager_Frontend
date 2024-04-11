21e389abb54f202c09cb1e032ca012e87fa0cc9e
Erika90 <erikilla.pinzon@email.com>
2024-04-11 12:01:22 -0400

    update task list

--- task.js
+++ task.js
@@ -1,14 +1,12 @@
- // Escuchar el envío del formulario para agregar una nueva tarea
- document.getElementById("task-form").addEventListener("submit", function(event) {
+document.getElementById("task-form").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevenir el envío del formulario por defecto
 
     // Obtener los valores del formulario de nueva tarea
     const title = document.getElementById("task-title").value;
     const description = document.getElementById("task-description").value;
-console.log("title ", title);
-console.log("description ",description);
+
     // Especificar la URL del endpoint para crear una nueva tarea
-    const createTaskUrl = "https://task1manager-7ffc650e7081.herokuapp.com/api/task"; // Reemplaza con la URL de tu backend para crear tarea
+    const createTaskUrl = "https://task1manager-7ffc650e7081.herokuapp.com/api/task";
 
     // Hacer una solicitud POST al backend para agregar una nueva tarea
     fetch(createTaskUrl, {
@@ -22,10 +20,23 @@ console.log("description ",description);
     .then(response => response.json())
     .then(data => {
         // Manejar la respuesta del backend
-        console.log(data); // Puedes hacer algo con la respuesta, como actualizar la lista de tareas en el frontend
+        console.log(data);
+
+        // Crear un nuevo elemento de lista para la tarea
+        const taskItem = document.createElement("li");
+        taskItem.textContent = data.title; // Suponiendo que la respuesta del servidor contiene el título de la tarea
+
+        // Agregar la tarea a la lista de tareas
+        const tasksList = document.getElementById("tasks");
+        tasksList.appendChild(taskItem);
+
+        // Limpiar los campos del formulario después de agregar la tarea
+        document.getElementById("task-title").value = "";
+        document.getElementById("task-description").value = "";
     })
     .catch(error => {
         console.error("Error:", error); // Manejar errores de la solicitud
     });
 });
+
 
